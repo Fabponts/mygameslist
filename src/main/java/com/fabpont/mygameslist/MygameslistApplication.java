28aa70c3d3d12d970f19f5950dcca415ade16e59
Fabpont <fabriciopontes02.fa@gmail.com>
2025-05-20 13:04:34 -0300

    Project created

---
+++ src/main/java/com/fabpont/mygameslist/MygameslistApplication.java
@@ -0,0 +1,13 @@
+package com.fabpont.mygameslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygameslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygameslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabpont/mygameslist/MygameslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabpont.mygameslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygameslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
